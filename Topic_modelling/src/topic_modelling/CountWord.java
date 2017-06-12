25a683be525f0345af6801e18e7c04a1cdbe9bf2
soong96 <15027493@imail.sunway.edu.my>
2017-06-12 16:28:30 +0800

    tfidf + stopping + stemming

--- Topic_modelling/src/topic_modelling/CountWord.java
+++ Topic_modelling/src/topic_modelling/CountWord.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,14 +19,17 @@ import java.util.Scanner;
  */
 public class CountWord {
     private HashMap<String, Object> mapAll = new HashMap<String, Object>();
-    private HashMap<String, Integer> map = new HashMap<String, Integer>();
+    private HashMap<String, Double> map = new HashMap<String, Double>();
     private HashMap<String, Double> mapTFIDF = new HashMap<String, Double>();
     private HashMap<String, Object> mapAllTFIDF = new HashMap<String, Object>();
+    private LinkedList<String> stopwords = new LinkedList<String>();
     private static final char DEFAULT_SEPARATOR = ',';
     
     public void calcWordCount() throws FileNotFoundException{
         // TODO code application logic here
         Scanner sc2 = null;
+        stopWords();
+        Stemmer stem= new Stemmer();
         for(int y = 1; y < 11; y++){
             String b = "doc"+y;
             try {                
@@ -40,8 +42,11 @@ public class CountWord {
                 while (s2.hasNext()) {
                     String s = s2.next();
                     String[] words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s");
-                    if(!map.containsKey(words[0])){
-                        map.put(words[0],0);
+                    words[0] = stem.stem(words[0]);
+                    if(!stopwords.contains(words[0])){
+                        if(!map.containsKey(words[0])){
+                            map.put(words[0],0.0);
+                        }
                     }
                 }
             }
@@ -60,41 +65,44 @@ public class CountWord {
                 while (s2.hasNext()) {
                     String s = s2.next();
                     String[] words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s");
-
-                    if(map.containsKey(words[0])){
-                        int i = map.get(words[0])+1;
-                        map.put(words[0],i);
+                    words[0] = stem.stem(words[0]);
+                    if(!stopwords.contains(words[0])){
+                        if(map.containsKey(words[0])){
+                            double i = map.get(words[0])+1;
+                            map.put(words[0],i);
+                        }
+                        else{
+                            map.put(words[0],1.0);
+                        }      
                     }
-                    else{
-                        map.put(words[0],1);
-                    }                               
-
                 }
+                
             }
-            int first = 0;
+            double first = 0;
             for(Map.Entry m:map.entrySet()){  
-                if(first < (int)m.getValue()){
-                    first = (int) m.getValue();
+                if(first < (double)m.getValue()){
+                    first = (double) m.getValue();
                 }                            
             }
+            
             //normalizeMap(first);
             System.out.println(map);
             System.out.println("\n\n");
             mapAll.put(b,map.clone());
-            map.replaceAll((k,v) -> 0);
+            map.replaceAll((k,v) -> 0.0);
         }
        
         for(Map.Entry m:mapAll.entrySet()){  
             map.putAll((HashMap)m.getValue());
             for(Map.Entry m2:map.entrySet()){  
-                if((int)m2.getValue() == 0){
+                if((double)m2.getValue() == 0){
                     mapTFIDF.put(m2.getKey().toString(), 0.0);
                 }
                 else
                     mapTFIDF.put(m2.getKey().toString(), calcTFIDF(m2.getKey().toString()));                
             }
             mapAllTFIDF.put(m.getKey().toString(),mapTFIDF.clone());
-            mapTFIDF.clear();
+            mapTFIDF.replaceAll((k,v) -> 0.0);
            
         }
         writeCSV();
@@ -104,7 +112,7 @@ public class CountWord {
         PrintWriter pw = new PrintWriter(new File("matrix.csv"));
         StringBuilder sb = new StringBuilder();
         sb.append(" "+DEFAULT_SEPARATOR);
-        for(Map.Entry m:map.entrySet()){          
+        for(Map.Entry m:mapTFIDF.entrySet()){          
             writeLine(sb,m.getKey().toString());           
         }
         sb.append("\n");
@@ -131,9 +139,9 @@ public class CountWord {
         sb.append(word + DEFAULT_SEPARATOR);
     }
     
-    public void normalizeMap(int largest){
+    public void normalizeMap(double largest){
          for(Map.Entry m:map.entrySet()){               
-             int calculation = (int)m.getValue()/largest;
+             double calculation = (double)m.getValue()/largest;
              String key = (String)m.getKey();
              map.put(key, calculation);
              
@@ -141,8 +149,8 @@ public class CountWord {
     }
     
     public double calcIDF(String term){
-        int n =0;
-        HashMap<String, Integer> hash = new HashMap<String, Integer>();
+        double n =0;
+        HashMap<String, Double> hash = new HashMap<String, Double>();
         for(Map.Entry m:mapAll.entrySet()){               
             String key = (String)m.getKey();
             hash.putAll((HashMap)mapAll.get(key));
@@ -151,15 +159,18 @@ public class CountWord {
             }
             hash.clear();
         }
-        return Math.log(mapAll.size()/n);
+        
+        double numDocs = mapAll.size();
+        double result = numDocs/n;
+        return Math.log(result);
     }
     
     public double calcTF(String term){
-        int termFreq = map.get(term);
-        int totalTerm = 0;
+        double termFreq = map.get(term);
+        double totalTerm = 0;
 
         for(Map.Entry m:map.entrySet()){            
-            totalTerm = totalTerm + (int)m.getValue();       
+            totalTerm = totalTerm + (double)m.getValue();       
      
         }
         return termFreq/totalTerm;
@@ -169,5 +180,23 @@ public class CountWord {
         
         return calcTF(term)*calcIDF(term);
         
+    }
+    
+    public void stopWords(){
+        Scanner sc2 = null;
+        try {                
+            sc2 = new Scanner(new File("src\\topic_modelling\\stopwords.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  
+        }
+        while (sc2.hasNextLine()) {
+                Scanner s2 = new Scanner(sc2.nextLine());
+            while (s2.hasNext()) {
+                String s = s2.next();
+                String[] words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s");
+                stopwords.add(words[0]);   
+                
+            }
+        }
     }
 }
